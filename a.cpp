9a978a25c4f7b6201d3824b6b460be1aea6a5c95
SUPERustam <498rustam@gmail.com>
2021-12-10 20:13:27 +0300

    🧞 New camp in every machine!

---
+++ a.cpp
@@ -0,0 +1,32 @@
+#include <algorithm> // special functions
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#define pb push_back
+#define in insert
+#define sz(x) (long long)x.size()
+#define all(x) x.begin(), x.end()
+#define ll long long
+
+using namespace std;
+
+ll ans, n, m, h, k, p, q, t, a, b, c, d, x, y, z;
+signed main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  vector<ll> a, b;
+  cin >> n >> m;
+  for (ll i = 0; i < n; ++i) {
+    cin >> c;
+    a.pb(c);
+  }
+  for (ll i = 0; i < m; ++i) {
+    cin >> c;
+    b.pb(c);
+  }
+
+  return 0;
+}
---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <algorithm> // special functions
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#define pb push_back
+#define in insert
+#define all(x) x.begin(), x.end()
+#define ll long long
+
+using namespace std;
+
+ll ans, n, m, h, k, p, q, t, a, b, c, d, e, x, y, z;
+signed main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  vector<ll> v;
+  cin >> n >> m;
+  cout << ans;
+  return 0;
+}
